58a3c6cd3385f1e1d62dfae88543c4e863ded10c
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-07 00:34:18 +0530

    Refactor Code to handle DRY violation on CSV Build

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -13,12 +13,28 @@ import java.util.stream.StreamSupport;
 public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(IndiaCensusCSV.class);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            Iterator<IndiaCensusCSV> censusCSVIterator = this.getCSVFileIterator(reader, IndiaCensusCSV.class);
             Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
+            int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+            return numOfEntries;
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }  catch (RuntimeException e) {
+            CensusAnalyserException.ExceptionType type = null;
+            if (e.getMessage().contains("CsvRequiredFieldEmptyException")) {
+                type = CensusAnalyserException.ExceptionType.CENSUS_DELIMITER_PROBLEM;
+            } else if (e.getMessage().contains("Error capturing CSV header!")) {
+                type = CensusAnalyserException.ExceptionType.CENSUS_HEADER_PROBLEM;
+            }
+            throw new CensusAnalyserException(e.getMessage(), type);
+        }
+    }
+
+    public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            Iterator<IndiaStateCodeCSV> stateCsvIterable = this.getCSVFileIterator(reader, IndiaStateCodeCSV.class);
+            Iterable<IndiaStateCodeCSV> csvIterable = () -> stateCsvIterable;
             int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
             return numOfEntries;
         } catch (IOException e) {
@@ -38,30 +54,16 @@ public class CensusAnalyser {
         }
     }
 
-    public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+    private <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndiaStateCodeCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaStateCodeCSV> censusCSVIterator = csvToBean.iterator();
-            Iterable<IndiaStateCodeCSV> csvIterable = () -> censusCSVIterator;
-            int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
-            return numOfEntries;
-        } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return csvToBean.iterator();
         } catch (IllegalStateException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
-        } catch (RuntimeException e) {
-            CensusAnalyserException.ExceptionType type = null;
-            if (e.getMessage().contains("CsvRequiredFieldEmptyException")) {
-                type = CensusAnalyserException.ExceptionType.CENSUS_DELIMITER_PROBLEM;
-            } else if (e.getMessage().contains("Error capturing CSV header!")) {
-                type = CensusAnalyserException.ExceptionType.CENSUS_HEADER_PROBLEM;
-            }
-            throw new CensusAnalyserException(e.getMessage(), type);
         }
     }
 }
