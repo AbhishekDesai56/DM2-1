9f7d8bd78e4a277858370a69f038e9c49906df42
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-06 23:59:13 +0530

    Added new function India State CSV

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -37,4 +37,31 @@ public class CensusAnalyser {
             throw new CensusAnalyserException(e.getMessage(), type);
         }
     }
+
+    public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaStateCodeCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaStateCodeCSV> censusCSVIterator = csvToBean.iterator();
+            Iterable<IndiaStateCodeCSV> csvIterable = () -> censusCSVIterator;
+            int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+            return numOfEntries;
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        } catch (RuntimeException e) {
+            CensusAnalyserException.ExceptionType type = null;
+            if (e.getMessage().contains("CsvRequiredFieldEmptyException")) {
+                type = CensusAnalyserException.ExceptionType.CENSUS_DELIMITER_PROBLEM;
+            } else if (e.getMessage().contains("Error capturing CSV header!")) {
+                type = CensusAnalyserException.ExceptionType.CENSUS_HEADER_PROBLEM;
+            }
+            throw new CensusAnalyserException(e.getMessage(), type);
+        }
+    }
 }
